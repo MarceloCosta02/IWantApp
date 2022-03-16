3ae1df7cc08eb48ac1d35e38f165768e9ffeb5ae
Marcelo Ponte <marcelo.ponte@capgemini.com>
2022-03-16 18:07:11 -0300

    Adicionando endpoints e validacoes para category

---
+++ IWantApp.Domain/Models/Products/Category.cs
@@ -0,0 +1,40 @@
+﻿using Flunt.Validations;
+
+namespace IWantApp.Domain.Models.Products;
+
+public class Category : Entity
+{
+    public string Name { get; private set; }
+    public bool Active { get; private set; }
+
+    public Category(string name, string createdBy, string editedBy)
+    {
+        Name = name;
+        Active = true;
+        CreatedBy = createdBy;
+        EditedBy = editedBy;
+        CreatedOn = DateTime.Now;
+        EditedOn = DateTime.Now;
+
+        Validate();
+    }
+
+    public void EditInfo(string name, bool active)
+    {
+        Active = active;
+        Name = name;
+
+        Validate();
+    }
+
+    private void Validate()
+    {
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(Name, "Name", "Name is required")
+            .IsGreaterOrEqualsThan(Name, 3, "Name")
+            .IsNotNullOrEmpty(CreatedBy, "CreatedBy", "CreatedBy is required")
+            .IsNotNullOrEmpty(EditedBy, "EditedBy", "EditedBy is required");
+
+        AddNotifications(contract);
+    }
+}
---
+++ IWantApp.Domain/Request/CategoryRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace IWantApp.Domain.Request;
+
+public class CategoryRequest
+{
+    public string Name { get; set; }
+    public bool Active { get; set; }
+}
