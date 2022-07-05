6e0f42a91f3b35f6b8d6dea6ec1f9e8c6fe5d562
Marcelo Ponte <marcelo.ponte@capgemini.com>
2022-07-05 16:09:48 -0300

    Adicionando paginacao e filtro na busca de vitrine de produtos

--- src/Endpoints/Products/ProductGetShowCase.cs
+++ src/Endpoints/Products/ProductGetShowCase.cs
@@ -13,11 +13,19 @@ public static class ProductGetShowCase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static async Task<IResult> Action(ApplicationDbContext context)
+    public static async Task<IResult> Action(int page = 1, int row = 1, string orderBy = "name", ApplicationDbContext context = null)
     {
-        var products = context.Products.Include(p => p.Category)
-            .Where(p => p.HasStock && p.Category.Active)
-            .OrderBy(p => p.Name).ToList();      
+        var queryBase = context.Products.Include(p => p.Category)
+            .Where(p => p.HasStock && p.Category.Active);
+
+        if (orderBy == "name")
+            queryBase = queryBase.OrderBy(p => p.Name);
+        else
+            queryBase = queryBase.OrderBy(p => p.Price);
+
+        var queryFilter = queryBase.Skip((page - 1) * row).Take(row);
+        
+        var products = queryFilter.ToList();
 
         var results = products.Select(p => new ProductResponse(p.Id, p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active));
 
